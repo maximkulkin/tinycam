281f9bfb8260257a1d8e216106670fc81207bfcb
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-02-15 05:12:56 -0500

    Improve rendering of XY grid
    Better control over line thicknesses
    Smooth fade in/out of minor grid lines

--- tinycam/ui/renderables/grid_xy.py
+++ tinycam/ui/renderables/grid_xy.py
@@ -1,6 +1,7 @@
 import math
 import moderngl
 import numpy as np
+from tinycam.types import Vector3
 from tinycam.ui.canvas import Context, Renderable, RenderState
 
 
@@ -40,45 +41,70 @@ class GridXY(Renderable):
                 out vec4 fragColor;
 
                 uniform float scale;
+                uniform float subscale;
                 uniform mat4 mvp_matrix;
                 uniform vec2 screen_size;
 
+                const float axes_width = 1.0;
+                const float grid_major_width = 1.0;
+                const float grid_minor_width = 1.0;
+                const vec4 grid_major_color = vec4(0.2, 0.2, 0.2, 1.0);
+                const vec4 grid_minor_color = vec4(0.1, 0.1, 0.1, 1.0);
+
+                vec4 normalize(vec4 v) {
+                    return v / max(v.w, 1e-6);
+                }
+
                 float grid(vec3 pos, float scale) {
-                    vec2 coord = pos.xy * scale;
-                    vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord);
-                    return 1 - min(min(grid.x, grid.y), 1.0);
+                    float closest_x = round(pos.x * scale) / scale;
+                    float closest_y = round(pos.y * scale) / scale;
+
+                    vec3 closest_grid_point = pos;
+                    if (abs(closest_x - pos.x) < abs(closest_y - pos.y)) {
+                        closest_grid_point.x = closest_x;
+                    } else {
+                        closest_grid_point.y = closest_y;
+                    }
+
+                    vec2 screen_pos = normalize(mvp_matrix * vec4(pos, 1.0)).xy;
+                    vec2 screen_closest_grid_point = normalize(mvp_matrix * vec4(closest_grid_point, 1.0)).xy;
+
+                    return length(screen_pos - screen_closest_grid_point) * screen_size.y;
                 }
 
                 void main() {
                     float t = -near_point.z / (far_point.z - near_point.z);
-                    vec3 fragPos = near_point + t * (far_point - near_point);
-                    float w = 4.0 / max(screen_size.x, screen_size.y) * 0.5;
-
                     if (t <= 0)
                         discard;
 
+                    vec3 fragPos = near_point + t * (far_point - near_point);
+
+                    gl_FragDepth = fragPos.z;
+
                     fragColor = vec4(0);
 
-                    float a = grid(fragPos, 0.01 * scale);
-                    if (a > 0) {
-                        fragColor += vec4(0.2, 0.2, 0.2, a * 0.1);
+                    if (grid(fragPos, 0.1 * scale) <= grid_major_width) {
+                        fragColor = grid_major_color;
+                    } else if(grid(fragPos, 1.0 * scale) <= grid_minor_width) {
+                        fragColor = vec4(grid_minor_color.xyz, subscale);
                     }
 
-                    a = grid(fragPos, 0.1 * scale);
-                    if (a > 0) {
-                        fragColor += vec4(0.2, 0.2, 0.2, a * 0.1);
-                    }
+                    if (abs(fragPos.x) < abs(fragPos.y)) {
+                        vec4 p1 = normalize(mvp_matrix * vec4(fragPos, 1.0));
+                        vec4 p2 = normalize(mvp_matrix * vec4(0, fragPos.y, fragPos.z, 1.0));
 
-                    a = grid(fragPos, 1 * scale);
-                    if (a > 0) {
-                        fragColor += vec4(0.4, 0.4, 0.4, a * 0.2);
-                    }
+                        float d = length(p1.xy - p2.xy) * screen_size.y;
+                        if (d <= 1.0) {
+                            fragColor.xyz = vec3(0, 1, 0);
+                        }
+                    } else {
+                        vec4 p1 = normalize(mvp_matrix * vec4(fragPos, 1.0));
+                        vec4 p2 = normalize(mvp_matrix * vec4(fragPos.x, 0, fragPos.z, 1.0));
 
-                    if (abs(fragPos.y) * scale < 0.1 && abs(fragPos.y) < abs(fragPos.x)) {
-                        fragColor.xyz = vec3(1, 0, 0);
-                    }
-                    if (abs(fragPos.x) * scale < 0.1 && abs(fragPos.x) < abs(fragPos.y)) {
-                        fragColor.xyz = vec3(0, 1, 0);
+                        float d = length(p1.xy - p2.xy) * screen_size.y;
+                        if (d <= 1.0) {
+                            fragColor.xyz = vec3(1, 0, 0);
+                        }
                     }
                 }
             '''
@@ -98,8 +124,13 @@ class GridXY(Renderable):
         mvp = state.camera.projection_matrix * state.camera.view_matrix
 
         self._program['mvp_matrix'].write(mvp.astype('f4').tobytes())
-        self._program['scale'] = pow(10, 1 - int(math.log(abs(state.camera.position.z * 0.25), 10)))
+        d = Vector3.dot(state.camera.rotation * state.camera.FORWARD, Vector3(0, 0, 1))
+        if d != 0:
+            d = state.camera.position.z / d
+        self._program['scale'] = pow(10, 0.4 - int(math.log(abs(d * 0.25), 10)))
+        scale_fraction, _ = math.modf(math.log(abs(d * 0.25), 10))
+        self._program['subscale'] = 1.0 - scale_fraction
         self._program['screen_size'] = state.camera.pixel_size
 
-        with self.context.scope(enable=moderngl.DEPTH_TEST, disable=moderngl.CULL_FACE):
+        with self.context.scope(enable=moderngl.DEPTH_TEST | moderngl.BLEND, disable=moderngl.CULL_FACE):
             self._vao.render(moderngl.TRIANGLE_STRIP)
