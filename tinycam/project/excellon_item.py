e74cde23dafecc669c6754e21c6fe291b5ff05af
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-02-21 18:36:42 -0500

    Add offset and scale properties to ExcellonItem

--- tinycam/project/excellon_item.py
+++ tinycam/project/excellon_item.py
@@ -4,6 +4,8 @@ import os.path
 from tinycam.formats import excellon
 from tinycam.globals import GLOBALS
 from tinycam.project.item import CncProjectItem
+from tinycam.types import Vector2
+from tinycam.properties import Vector2Property
 
 
 class ExcellonItem(CncProjectItem):
@@ -11,6 +13,16 @@ class ExcellonItem(CncProjectItem):
         super().__init__(name, color=QtGui.QColor.fromRgbF(0.65, 0.0, 0.0, 0.6))
         self._excellon_file = excellon_file
         self._geometry = self._excellon_file.geometry
+
+        self._offset = Vector2(0, 0)
+        self._scale = Vector2(1, 1)
+
+    def _update(self):
+        self._update_geometry()
+        self._signal_updated()
+
+    offset = Vector2Property(on_update=_update)
+    scale = Vector2Property(on_update=_update)
 
     def clone(self):
         clone = ExcellonItem(self.name, self._excellon_file)
@@ -37,6 +49,9 @@ class ExcellonItem(CncProjectItem):
     @geometry.setter
     def geometry(self, value):
         self._geometry = value
+
+    def _update_geometry(self):
+        pass
 
     @staticmethod
     def from_file(path):
