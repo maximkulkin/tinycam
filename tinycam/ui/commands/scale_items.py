1ead6136a1db9997479955ac6b9c8f21405147b0
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-02-25 08:03:01 -0800

    Remove obsolete Point class

--- tinycam/ui/commands/scale_items.py
+++ tinycam/ui/commands/scale_items.py
@@ -1,11 +1,11 @@
 from PySide6 import QtGui
 
 from tinycam.globals import GLOBALS
-from tinycam.ui.utils import Point
+from tinycam.types import Vector2
 
 
 class ScaleItemsCommand(QtGui.QUndoCommand):
-    def __init__(self, items, scale, offset=Point(0.0, 0.0), parent=None):
+    def __init__(self, items, scale: float, offset=Vector2(0.0, 0.0), parent=None):
         super().__init__('Scale', parent=parent)
         self._items = items
         self._scale = scale
@@ -22,5 +22,5 @@ class ScaleItemsCommand(QtGui.QUndoCommand):
         for item in self._items:
             item.geometry = GLOBALS.GEOMETRY.scale(
                 GLOBALS.GEOMETRY.translate(item.geometry, -self._offset),
-                1.0/self._scale
+                1.0 / self._scale
             )
