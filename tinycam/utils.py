19dd857951afa981372042e96e5b19a9fae6c00e
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-02-25 07:58:08 -0800

    Fix indent in utils.py

--- tinycam/utils.py
+++ tinycam/utils.py
@@ -1,14 +1,14 @@
 def index_if(items, predicate, missing=-1):
-  for index, item in enumerate(items):
-    if predicate(item):
-      return index
+    for index, item in enumerate(items):
+        if predicate(item):
+            return index
 
-  return missing
+    return missing
 
 
 def find_if(items, predicate, missing=None):
-  for item in items:
-    if predicate(item):
-      return item
+    for item in items:
+        if predicate(item):
+            return item
 
-  return missing
+    return missing
