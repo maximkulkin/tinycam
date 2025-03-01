8ab9cb9fd132b9973d363797aec29a09c88052c9
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-02-28 19:26:29 -0500

    Add explicit list of exports in tinycam.project to make linter happy

--- tinycam/project/__init__.py
+++ tinycam/project/__init__.py
@@ -3,3 +3,14 @@ from tinycam.project.item import CncProjectItem
 from tinycam.project.excellon_item import ExcellonItem
 from tinycam.project.gerber_item import GerberItem
 from tinycam.project.jobs import CncJob, CncIsolateJob, CncDrillJob
+
+
+__all__ = [
+    'CncProject',
+    'CncProjectItem',
+    'GerberItem',
+    'ExcellonItem',
+    'CncJob',
+    'CncIsolateJob',
+    'CncDrillJob',
+]
