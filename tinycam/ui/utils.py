54f93b66973a0e01ce0c099c62dc55391bb56194
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-03-04 11:47:45 -0500

    Fix types in utils.py

--- tinycam/ui/utils.py
+++ tinycam/ui/utils.py
@@ -1,8 +1,8 @@
 from PySide6 import QtGui
 from itertools import chain
-from tinycam.types import Vector2, Vector3, Vector4
+from tinycam.types import Vector2, Vector3, Vector4, Matrix44
 from tinycam.ui.camera import Camera
-from typing import Tuple
+from typing import cast
 
 
 def qcolor_to_vec4(color: QtGui.QColor) -> Vector4:
@@ -10,18 +10,18 @@ def qcolor_to_vec4(color: QtGui.QColor) -> Vector4:
 
 
 def project(point: Vector3, camera: Camera) -> Vector2:
-    sp = camera.projection_matrix * camera.view_matrix * Vector4(*point, 1.0)
+    sp = camera.projection_matrix * camera.view_matrix * Vector4.from_vector3(point, 1.0)
     return camera.ndc_to_screen_point(sp)
 
 
-def unproject(point: Tuple[float, float], camera: Camera) -> Vector3:
-    vp = camera.projection_matrix * camera.view_matrix
+def unproject(point: tuple[float, float], camera: Camera) -> Vector3:
+    vp = cast(Matrix44, camera.projection_matrix * camera.view_matrix)
     ivp = vp.inverse
 
     ndc = camera.screen_to_ndc_point(Vector2(point))
 
-    p = Vector4(*ndc, 1)
-    v: Vector4 = ivp * p
+    p = Vector4(ndc[0], ndc[1], ndc[2], 1)
+    v: Vector4 = cast(Vector4, ivp * p)
     if v.w != 0.0:
         v /= v.w
 
