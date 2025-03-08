6be28d1cf142969e4bfc418811393279e150360f
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-03-08 01:35:54 -0500

    Fix type annotations in SelectTool

--- tinycam/ui/tools/select.py
+++ tinycam/ui/tools/select.py
@@ -1,15 +1,20 @@
 import enum
+from typing import cast
+
 import numpy as np
+from PySide6 import QtCore
+from PySide6.QtCore import QPoint, QPointF, QEvent, Qt
+from PySide6.QtGui import QMouseEvent, QKeyEvent
+from PySide6.QtWidgets import QWidget
+
 from tinycam.globals import GLOBALS
 from tinycam.types import Vector2, Vector4
 from tinycam.ui.tools import CncTool
 from tinycam.ui.view_items.canvas import Rectangle
 from tinycam.ui.view_items.project_item import CncProjectItemView
-from PySide6 import QtCore
-from PySide6.QtCore import Qt
 
 
-def vector2(point: QtCore.QPoint | QtCore.QPointF) -> Vector2:
+def vector2(point: QPoint | QPointF) -> Vector2:
     return Vector2(point.x(), point.y())
 
 
@@ -30,7 +35,7 @@ class SelectTool(CncTool):
 
     def cancel(self):
         if self._box:
-            self.view.remove(self._box)
+            self.view.remove_item(self._box)
             self._box = None
         self._p1 = None
         self._p2 = None
@@ -40,47 +45,56 @@ class SelectTool(CncTool):
         self.cancel()
         super().deactivate()
 
-    def eventFilter(self, widget: QtCore.QObject, event: QtCore.QEvent) -> bool:
-        if (event.type() == QtCore.QEvent.MouseButtonPress and
-                event.button() & Qt.LeftButton):
-            self._p1 = vector2(event.position())
+    def eventFilter(self, widget: QWidget, event: QEvent) -> bool:
+        mouse_event = cast(QMouseEvent, event)
+
+        if (event.type() == QEvent.MouseButtonPress and
+                mouse_event.button() & Qt.LeftButton):
+            self._p1 = vector2(mouse_event.position())
             self._p2 = self._p1
             self._selecting = True
             return True
         elif (event.type() == QtCore.QEvent.MouseButtonRelease and
-                event.button() & Qt.LeftButton):
+                mouse_event.button() & Qt.LeftButton):
 
-            modifiers = self._make_selection_modifiers(event.modifiers())
+            modifiers = self._make_selection_modifiers(mouse_event.modifiers())
             if self._box:
+                assert(self._p1 is not None)
+                assert(self._p2 is not None)
                 self._select_items_in_box(self._p1, self._p2, modifiers)
 
                 self.view.remove_item(self._box)
                 self._box = None
             else:
-                self._select_item_at_point(vector2(event.position()), modifiers)
+                self._select_item_at_point(vector2(mouse_event.position()), modifiers)
 
             self._selecting = False
             return True
-        elif self._selecting and event.type() == QtCore.QEvent.MouseMove:
-            self._p2 = vector2(event.position())
+        elif self._selecting and event.type() == QEvent.MouseMove:
+            self._p2 = vector2(mouse_event.position())
 
             if self._box is None and (self._p1 - self._p2).length > 10:
                 self._box = self._make_box()
                 self.view.add_item(self._box)
             elif self._box is not None:
                 self._box.center = (self._p1 + self._p2) * 0.5
-                self._box.size = np.abs(self._p1 - self._p2)
+                self._box.size = Vector2(np.abs(self._p1 - self._p2))
 
             widget.update()
             return True
-        elif event.type() == QtCore.QEvent.KeyPress:
-            if event.code() == Qt.Key_Escape:
+        elif event.type() == QEvent.KeyPress:
+            key_event = cast(QKeyEvent, event)
+            if key_event.code() == Qt.Key_Escape:
                 self.cancel()
                 return True
 
         return False
 
     def _make_box(self) -> Rectangle:
+        assert(self.view.ctx is not None)
+        assert(self._p1 is not None)
+        assert(self._p2 is not None)
+
         return Rectangle(
             context=self.view.ctx,
             center=(self._p1 + self._p2) * 0.5,
@@ -90,7 +104,7 @@ class SelectTool(CncTool):
             edge_width=2,
         )
 
-    def _select_item_at_point(self, point: QtCore.QPointF, modifiers: SelectionModifier):
+    def _select_item_at_point(self, point: Vector2, modifiers: SelectionModifier):
         project = GLOBALS.APP.project
 
         picked = self.view.pick_item(point)
