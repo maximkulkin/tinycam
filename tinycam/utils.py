209f0be8692f4664e890bedcfbc06b347e393b2a
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-05-14 03:05:46 -0400

    Add lerp utility function

--- tinycam/utils.py
+++ tinycam/utils.py
@@ -1,3 +1,30 @@
+from typing import Protocol, Self, overload, runtime_checkable
+
+
+@runtime_checkable
+class Lerpable(Protocol):
+    def __add__(self, other: Self) -> Self:
+        ...
+
+    def __sub__(self, other: Self) -> Self:
+        ...
+
+    def __mul__(self, other: float) -> Self:
+        ...
+
+
+@overload
+def lerp(a: int, b: int, t: float) -> float: ...  # noqa: E704
+@overload
+def lerp(a: float, b: float, t: float) -> float: ...  # noqa: E704
+@overload
+def lerp[T: Lerpable](a: T, b: T, t: float) -> T: ...  # noqa: E704
+
+
+def lerp(a, b, t):
+    return a + (b - a) * t
+
+
 def index_if(items, predicate, missing=-1):
     for index, item in enumerate(items):
         if predicate(item):
