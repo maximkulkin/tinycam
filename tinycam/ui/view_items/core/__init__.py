806ea19b143a71cf3a98eb5fdbf27b7027d53da8
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-08-30 01:49:32 -0400

    Add Billboard node

--- tinycam/ui/view_items/core/__init__.py
+++ tinycam/ui/view_items/core/__init__.py
@@ -2,6 +2,7 @@ from tinycam.ui.view_items.core.node import Node
 from tinycam.ui.view_items.core.node2d import Node2D
 from tinycam.ui.view_items.core.node3d import Node3D
 from tinycam.ui.view_items.core.debug import Debug
+from tinycam.ui.view_items.core.billboard import Billboard
 from tinycam.ui.view_items.core.infinite_grid_xy import InfiniteGridXY
 from tinycam.ui.view_items.core.line2d import Line2D
 from tinycam.ui.view_items.core.line3d import Line3D
@@ -14,6 +15,7 @@ __all__ = [
     'Node2D',
     'Node3D',
     'Debug',
+    'Billboard',
     'InfiniteGridXY',
     'Line2D',
     'Line3D',
---
+++ tinycam/ui/view_items/core/billboard.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from tinycam.types import Vector3, Matrix44
+from tinycam.ui.view import RenderState
+from tinycam.ui.view_items.core.node3d import Node3D
+
+
+class Billboard(Node3D):
+    """
+    A node that always rotates towards camera
+    so that it's XY plane is perpendicular to it.
+    """
+    def render(self, state: RenderState):
+        camera = state.camera
+
+        rotation = camera.rotation.conjugate
+        y_axis = rotation * camera.UP
+        z_axis = rotation * camera.BACKWARD
+        x_axis = Vector3.cross(y_axis, z_axis)
+
+        mat = Matrix44(np.array((
+            (x_axis[0], y_axis[0], z_axis[0], 0.),
+            (x_axis[1], y_axis[1], z_axis[1], 0.),
+            (x_axis[2], y_axis[2], z_axis[2], 0.),
+            (0, 0, 0, 1.),
+        )))
+        _, rotation, _ = mat.decompose()
+        self.world_rotation = rotation.conjugate
+
+        super().render(state)
