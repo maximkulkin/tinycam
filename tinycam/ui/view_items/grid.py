86ed037484cca1dbc2a035d13fcef5b84e084c5a
Maxim Kulkin <maxim.kulkin@gmail.com>
2025-09-05 00:44:15 -0400

    Update Grid with dimensions text

--- tinycam/ui/view_items/grid.py
+++ tinycam/ui/view_items/grid.py
@@ -1,9 +1,12 @@
+from PySide6 import QtGui
+from PySide6.QtCore import Qt
 import numpy as np
 
-from tinycam.types import Vector2, Vector4
+from tinycam.types import Vector2, Vector3, Vector4
 from tinycam.ui.view import Context
 from tinycam.ui.view_items.core.line2d import Line2D
 from tinycam.ui.view_items.core.node3d import Node3D
+from tinycam.ui.view_items.core.text import FontAtlas, Text
 
 type any_float = float | np.float32
 
@@ -27,8 +30,52 @@ class Grid(Node3D):
             self._line((0, y), (size.x, y))
             y += grid_spacing.y
 
-        self._line((0, 0), (size.x, 0), color=Vector4(1, 0, 0, 1))
-        self._line((0, 0), (0, size.y), color=Vector4(0, 1, 0, 1))
+        self._line((0, 0), (size.x + grid_spacing.x * 0.5, 0), color=Vector4(1, 0, 0, 1))
+        self._line((0, 0), (0, size.y + grid_spacing.y * 0.5), color=Vector4(0, 1, 0, 1))
+
+        font = QtGui.QFont('Arial')
+        font.setPointSize(48)
+        self._font_atlas = FontAtlas.create(self.context, font, '0123456789.XY')
+
+        x = grid_spacing.x
+        while x <= size.x:
+            self._text(
+                (x, 0),
+                str(x),
+                Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop,
+            ) 
+
+            x += grid_spacing.x
+
+        y = grid_spacing.y
+        while y <= size.y:
+            self._text(
+                (0, y),
+                str(y),
+                Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter,
+            )
+
+            y += grid_spacing.y
+
+        self._text(
+            (0, 0),
+            '0',
+            Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop,
+        )
+
+        self._text(
+            (size.x + grid_spacing.x * 0.5, 0),
+            'X',
+            color=Vector4(1, 0, 0, 1),
+            alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter,
+        )
+
+        self._text(
+            (0, size.y + grid_spacing.y * 0.5),
+            'Y',
+            color=Vector4(0, 1, 0, 1),
+            alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom,
+        )
 
     def _line(
         self,
@@ -38,3 +85,22 @@ class Grid(Node3D):
     ):
         self.add_child(Line2D(self.context, [Vector2(p1), Vector2(p2)], color=color))
 
+    def _text(
+        self,
+        p: Vector2 | tuple[any_float, any_float],
+        text: str,
+        alignment: Qt.AlignmentFlag = Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop,
+        color: Vector4 = Vector4(0.4, 0.4, 0.4, 1.0),
+        margin: int | float | Vector2 = 20,
+    ):
+        text_node = Text(
+            self.context,
+            self._font_atlas,
+            text,
+            color=color,
+            alignment=alignment,
+            margin=margin,
+        )
+        text_node.position = Vector3.from_vector2(Vector2(p))
+        text_node.scale = Vector3(0.06, 0.06, 1.0)
+        self.add_child(text_node)
